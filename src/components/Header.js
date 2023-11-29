6016bd1bb2f200de73f6b9ebe603cb9110a7860e
Bigom <bigom2199@gmail.com>
2023-11-29 17:12:33 +0100

    all workshop

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = () => {
+    return(
+        <header className="container-fluid bg-danger text-white pt-5 pb-5">
+            <div className="container">
+                <h2>Marketplace UI</h2>
+            </div>
+        </header>
+    )
+}
+export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,22 +27,27 @@ const NavbarLink = " nav-item" >
    return (
     <nav className = "navbar navbar-expand-sm bg-dark navbar-dark" >
         <div className = "container-fluid" >
-            <a className = "navbar-brand" href ="#" >
-                
-            </a>
-            <button className ="navbar-togger"
-            type="button"
-            data-bs-toggle="collapse"
+            <a className = "navbar-brand" href ="#" >Logo </a>
+            <button className ="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#collapsibleNavbar">
-              <span className="navbar-toggler-icon">
-
-              </span>
+              <span className="navbar-toggler-icon"> </span>
             </button>
             <div className ="collapse navbar-collapse" id="collapsibleNavbar">
               <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">link</a>
+
+
+                </li>
+                <li className ="nav-item">
+                  <a className="nav-link" href="#"></a>
+                </li>
+
+
                 
               </ul>
             </div>
         </div>
     </nav>
-   )
+        )
+           export default App;
